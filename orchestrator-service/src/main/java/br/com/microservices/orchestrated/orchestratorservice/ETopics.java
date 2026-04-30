0995bbc9c04718a0829fa23844da6aed94f144d8
Gabriel Soares <gabrielsoacc@gmail.com>
2026-04-30 17:38:14 -0300

    fix: removed unused lombok imports

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/ETopics.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/ETopics.java
@@ -1,8 +1,6 @@
 package br.com.microservices.orchestrated.orchestratorservice;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 public enum ETopics {

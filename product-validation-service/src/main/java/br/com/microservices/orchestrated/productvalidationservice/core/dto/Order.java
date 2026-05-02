9bc03f66c42090266b2067f3e5c3679d99e5ac59
Gabriel Soares <gabrielsoacc@gmail.com>
2026-05-02 13:23:32 -0300

    feat: added microservices to compose

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/Order.java
@@ -0,0 +1,25 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.dto;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    private String id;
+    private List<Product> products;
+    private LocalDateTime createdAt;
+    private String transactionalId;
+
+    private double totalAmount;
+    private int totalItems;
+}

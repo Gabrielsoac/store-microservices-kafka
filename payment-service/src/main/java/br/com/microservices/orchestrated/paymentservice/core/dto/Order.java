55ed9fbf1fc7d639e9f3d6ae3fa9faa6cac97d22
Gabriel Soares <gabrielsoacc@gmail.com>
2026-05-02 13:29:39 -0300

    feat: created dtos for saga and entity domains in payment microservice

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Order.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/Order.java
@@ -1,6 +1,5 @@
-package br.com.microservices.orchestrated.productvalidationservice.core.dto;
+package br.com.microservices.orchestrated.paymentservice.core.dto;
 
-import br.com.microservices.orchestrated.orderservice.core.document.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
